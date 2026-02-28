ac7b89413f4c1831aa62cf2a444f6c9dcb5eaea6
Adara Noble <nobleadara1@gmail.com>
2026-02-28 16:36:25 -0700

    Movement Implementation

--- Objects/Character/Character.gd
+++ Objects/Character/Character.gd
@@ -2,37 +2,40 @@ extends Node
 
 @onready var camera = $Camera3D
 @onready var rayCast = $RayCast3D
-@export_range(0.0, 1.0) var sensitivity = 0.005
-
-#const SPEED = 5.0
-#const JUMP_VELOCITY = 4.5
+@onready var scene = $"../Hallway"
+@export_range(0.0, 1.0) var sensitivity = 0.1
+var _mouse_rotation : Vector3
+var _rotation_input : float
+var _tilt_input : float
+var _camera_rotation : Vector3
+@export_range(-90,0) var x_tilt_min : int = -45
+@export_range(0, 90) var x_tilt_max : int = 45 
+@export_range(-90,0) var y_tilt_min : int = -60
+@export_range(0, 90) var y_tilt_max : int = 60
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		var movement = Vector3(-event.relative.x, event.relative.y, 0) * sensitivity
-		var target_position = camera.position + movement
-
-		var plane_position = get_node("/root/Node3D/StaticBody3D").global_transform.origin
-		var min_x = plane_position.x - 1
-		var max_x = plane_position.x + 1
-		var min_y = plane_position.y - 1
-		var max_y = plane_position.y + 1
-		
-		var view_direction = -camera.global_transform.basis.z.normalized()
-		camera.position = Vector3(
-			clamp(target_position.x, min_x / target_position.z, max_x / target_position.z),
-			clamp(target_position.y, min_y / target_position.z, max_y / target_position.z),
-			target_position.z
-		)
-		camera.translate(view_direction * movement.z)
+		_rotation_input = -event.relative.x * sensitivity
+		_tilt_input = -event.relative.y * sensitivity
+		print(Vector2(_rotation_input,_tilt_input))
 
 func _physics_process(delta: float) -> void:
-	pass
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	_update_camera(delta)
 
-	#move_and_slide()
+func _update_camera(delta : float):
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, deg_to_rad(x_tilt_min), deg_to_rad(x_tilt_max))
+	_mouse_rotation.y += _rotation_input * delta
+	_mouse_rotation.y = clamp(_mouse_rotation.y, deg_to_rad(y_tilt_min), deg_to_rad(y_tilt_max))
+	
+	_camera_rotation = Vector3(_mouse_rotation.x,_mouse_rotation.y,0.0)
+	
+	camera.transform.basis = Basis.from_euler(_camera_rotation)
+	camera.rotation.z = 0.0
+	
+	
+	_rotation_input = 0.0
+	_tilt_input = 0.0
